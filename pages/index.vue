e0de2c47beb8abf70510c1a06ba07a4ed8c48b89
FelixGrafschmidt <drache.dragon@gmail.com>
2022-09-09 18:03:43 +0200

    scoreboard

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,25 @@
 <template>
-	<section class="mx-auto my-auto justify-center items-center" flex="~ row wrap" gap-8>
-		<div v-for="(song, i) in songs" :key="i" class="items-center justify-center" border-8 flex="~ col" w="1/6">
-			<div :class="{ 'filter-blur-md': !song.revealed }" class="text-3rem">
-				{{ (songs.indexOf(song) + 1).toString().padStart(2, "0") }}
+	<section class="mx-auto my-auto justify-center items-center text-lg [zoom:110%]" flex="~ row wrap" gap-8>
+		<div v-for="(song, i) in songs" :key="i" class="items-center justify-center" border-8 rounded flex="~ col" w="1/6" bg-gray-700>
+			<div class="flex flex-col items-center" :class="{ 'filter-blur-md': !song.revealed }" py-4 gap-2>
+				<div class="text-3rem">
+					{{ (songs.indexOf(song) + 1).toString().padStart(2, "0") }}
+				</div>
+				<div class="flex flex-col w-full items-center">
+					<div class="text-xl font-extrabold">{{ song.origin }}</div>
+					<div class="italic text-sm">{{ song.name }}</div>
+				</div>
 			</div>
-			<div class="flex flex-col w-full">
-				<div :class="{ 'filter-blur': !song.revealed }" class="text-xl font-extrabold">{{ song.origin }}</div>
-				<div :class="{ 'filter-blur': !song.revealed }" class="italic text-sm">{{ song.name }}</div>
-				<div class="">{{ song.genres.join(", ") }}</div>
+			<div class="py-4 flex flex-row flex-wrap gap-3 border-y-2 w-full px-4">
+				<span v-for="(genre, k) in song.genres" :key="k">{{ genre }}</span>
 			</div>
-			<div flex="~ row">
-				<div v-for="(player, j) in players" :key="j" class="w-5% px-2 flex flex-col justify-center items-center">
+			<div flex="~ row" gap-6 py-4>
+				<div v-for="(player, j) in players" :key="j" class="w-30% flex flex-col justify-center items-center gap-2">
 					{{ player.name }}
-					<span v-if="song.correct.includes(player.id)" :invisible="!song.revealed" class="text-3rem text-green">O</span>
-					<span v-else :invisible="!song.revealed" class="text-3rem text-red">X</span>
+					<span v-if="song.correct.includes(player.id)" :class="{ invisible: !song.revealed }" class="text-2rem text-green">
+						O
+					</span>
+					<span v-else :class="{ invisible: !song.revealed }" class="text-2rem text-red">X</span>
 				</div>
 			</div>
 		</div>
