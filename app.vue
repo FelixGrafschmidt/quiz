dbb2309f87327dd93aed1e036d0e1fe89cbfd0e4
FelixGrafschmidt <drache.dragon@gmail.com>
2023-02-27 16:11:46 +0100

    reopen web socket; update dependencies

--- app.vue
+++ app.vue
@@ -7,17 +7,23 @@
 <script setup lang="ts">
 	onMounted(() => {
 		try {
-			const ws = useRuntimeConfig().dev
-				? new WebSocket(`ws://${window.location.hostname}:4000/ws?channel=` + useRoute().query.game?.toString())
-				: new WebSocket(`wss://${window.location.hostname}/ws?channel=` + useRoute().query.game?.toString());
-			ws.onmessage = ({ data }) => {
-				const { key, id, value } = JSON.parse(data);
-				useStore().update(key, id, value);
-			};
+			setupWebSocket();
 		} catch (err) {
 			console.log(err);
 		}
 	});
+	function setupWebSocket() {
+		const ws = useRuntimeConfig().dev
+			? new WebSocket(`ws://${window.location.hostname}:4000/ws?channel=` + useRoute().query.game?.toString())
+			: new WebSocket(`wss://${window.location.hostname}/ws?channel=` + useRoute().query.game?.toString());
+		ws.onmessage = ({ data }) => {
+			const { key, id, value } = JSON.parse(data);
+			useStore().update(key, id, value);
+		};
+		ws.onclose = function () {
+			setTimeout(() => setupWebSocket(), 1000);
+		};
+	}
 </script>
 
 <style>
